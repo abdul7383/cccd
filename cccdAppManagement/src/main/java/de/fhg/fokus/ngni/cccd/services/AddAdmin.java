f78ae695393445a41b788284f633070b084979e1
Abdulrahman Hamood <abdul7383@yahoo.de>
2013-03-26 16:29:12 +0100

    new Java AddAmin and doc update

--- cccdAppManagement/src/main/java/de/fhg/fokus/ngni/cccd/services/AddAdmin.java
+++ cccdAppManagement/src/main/java/de/fhg/fokus/ngni/cccd/services/AddAdmin.java
@@ -1,11 +1,18 @@
 package de.fhg.fokus.ngni.cccd.services;
 
+import java.io.Console;
 import java.io.IOException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.security.authentication.encoding.PasswordEncoder;
 
+import com.mongodb.Mongo;
 
-public class Test {
+import de.fhg.fokus.ngni.cccd.model.User;
+
+
+public class AddAdmin {
 
 	/**
 	 * @param args
@@ -13,22 +20,21 @@ public class Test {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Console console = System.console();
+		String enteredUsername = new String(console.readLine("Please enter the admin username to be created: "));
+		String enteredPassword = new String(console.readPassword("Please enter the password: "));
+
 		PasswordEncoder passwordEncoder = new org.springframework.security.authentication.encoding.Md5PasswordEncoder();
-//		Mongo mongo=null;
-//		try {
-//			mongo = new Mongo("localhost:27017");
-//		} catch (UnknownHostException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		String encodedPassword = passwordEncoder.encodePassword("abdul",
-				"abdul");
+		String encodedPassword = passwordEncoder.encodePassword(enteredPassword,
+				enteredUsername);
 		System.out.println(encodedPassword);
-//		User user = new User("cccd1",encodedPassword,"Abdul","Hamood");
-//		user.setRole(1);
-//		MongoTemplate mongoTemplate = new MongoTemplate(mongo,"users");
-//		mongoTemplate.save(user);
+		User user = new User("cccd1",encodedPassword,"Abdul","Hamood");
+		user.setRole(1);
+		ApplicationContext context= new ClassPathXmlApplicationContext("cccd-config.xml");
+		Mongo mongoDb = (Mongo) context.getBean("mongoDb");
+		System.out.println(mongoDb);
+		System.exit(0);
+		//mongoTemplate.save(user);
 //		TransportClient esClient = new 
 //				TransportClient().addTransportAddress(new 
 //				InetSocketTransportAddress("localhost", 9300));
